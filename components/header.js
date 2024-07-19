8cd43ddef187401403558cf5c1749f7d0858da10
NutWatcher <lyy@nutwatcher.com>
2024-07-19 14:38:35 +0800

    head

---
+++ components/header.js
@@ -0,0 +1,131 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+const links = [
+    {
+        label: "Features",
+        href: "#Features",
+    },
+    {
+        label: "Pricing",
+        href: "#Pricing",
+    },
+    {
+        label: "Wall of Love",
+        href: "#WallOfLove",
+    },
+    {
+        label: "FAQ",
+        href: "#FAQ",
+    },
+];
+const siteConfig = {
+    name: "Notion Card",
+};
+export default function Home() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    return (
+        <header className="py-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <nav className="relative z-50 flex justify-between">
+                <div className="flex items-center md:gap-x-12">
+                    <Link
+                        href="/"
+                        aria-label="Landing Page Boilerplate"
+                        title="Landing Page Boilerplate"
+                        className="flex items-center space-x-1 font-bold"
+                    >
+                        <Image
+                            alt={siteConfig.name}
+                            src="/logo.svg"
+                            className="w-8 h-8"
+                            width={32}
+                            height={32}
+                        />
+                        <span className="text-gray-950 dark:text-gray-300 hidden md:block">
+                            {siteConfig.name}
+                        </span>
+                    </Link>
+                </div>
+
+                <ul className="hidden items-center gap-6 md:flex">
+                    {links.map((link) => (
+                        <li key={link.label}>
+                            <Link
+                                href={link.href}
+                                aria-label={link.label}
+                                title={link.label}
+                                className="tracking-wide transition-colors duration-200 font-norma"
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="md:hidden">
+                    <button
+                        aria-label="Open Menu"
+                        title="Open Menu"
+                        className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                        onClick={() => setIsMenuOpen(true)}
+                    ></button>
+                    {isMenuOpen && (
+                        <div className="absolute top-0 left-0 w-full z-50">
+                            <div className="p-5 bg-background border rounded shadow-sm">
+                                <div className="flex items-center justify-between mb-4">
+                                    <div>
+                                        <Link
+                                            href="/"
+                                            aria-label="Landing Page Boilerplate"
+                                            title="Landing Page Boilerplate"
+                                            className="inline-flex items-center"
+                                        >
+                                            <Image
+                                                alt={siteConfig.name}
+                                                src="/logo.svg"
+                                                className="w-8 h-8"
+                                                width={32}
+                                                height={32}
+                                            />
+                                            <span className="ml-2 text-xl font-bold tracking-wide text-gray-950 dark:text-gray-300">
+                                                {siteConfig.name}
+                                            </span>
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <button
+                                            aria-label="Close Menu"
+                                            title="Close Menu"
+                                            className="tracking-wide transition-colors duration-200 font-norma"
+                                            onClick={() => setIsMenuOpen(false)}
+                                        ></button>
+                                    </div>
+                                </div>
+                                <nav>
+                                    <ul className="space-y-4">
+                                        {links.map((link) => (
+                                            <li key={link.label}>
+                                                <Link
+                                                    href={link.href}
+                                                    aria-label={link.label}
+                                                    title={link.label}
+                                                    className="font-medium tracking-wide  transition-colors duration-200 hover:text-deep-purple-accent-400"
+                                                    onClick={() =>
+                                                        setIsMenuOpen(false)
+                                                    }
+                                                >
+                                                    {link.label}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </header>
+    );
+}
