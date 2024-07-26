d766d16f8be5df2bf7dd8b2d91743fcd6981968f
NutWatcher <lyy@nutwatcher.com>
2024-07-26 15:30:51 +0800

    增加google 分析

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
             >
                 <Header />
                 {children}
+                <GoogleAnalytics gtmId="G-S9EM6X9DCR" />
             </body>
         </html>
     );
